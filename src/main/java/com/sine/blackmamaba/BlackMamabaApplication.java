52c4246beb3ddb499ef7eafdb5cde8c07d8d1442
aelshal <anas.elshal@dxc.com>
2025-01-05 23:00:20 +0200

    intila commit

---
+++ src/main/java/com/sine/blackmamaba/BlackMamabaApplication.java
@@ -0,0 +1,13 @@
+package com.sine.blackmamaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackMamabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackMamabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sine/blackmamaba/BlackMamabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sine.blackmamaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackMamabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
